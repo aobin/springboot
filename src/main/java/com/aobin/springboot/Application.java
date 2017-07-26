cbd00c37b80cccb0e7be7e6382fcadc813d624c7
root <176777235@qq.com>
2017-07-26 07:48:08 -0700

    Initial commit

---
+++ src/main/java/com/aobin/springboot/Application.java
@@ -0,0 +1,18 @@
+package com.aobin.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by root on 7/26/17.
+ */
+
+@SpringBootApplication
+public class Application
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class,args);
+    }
+}
