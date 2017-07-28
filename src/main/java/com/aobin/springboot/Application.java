4f9de1ecaa8993b5637c204d007f8c0dd69e813a
aobin <176777235@qq.com>
2017-07-28 00:02:14 -0700

    package into a standalone jar.

--- src/main/java/com/aobin/springboot/Application.java
+++ src/main/java/com/aobin/springboot/Application.java
@@ -1,18 +1,28 @@
 package com.aobin.springboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by root on 7/26/17.
  */
 
-@SpringBootApplication
+//@SpringBootApplication
+@EnableAutoConfiguration
+@RestController
 public class Application
 {
 
+    @RequestMapping("/")
+    String home()
+    {
+        return "Hello World!";
+    }
+
     public static void main(String[] args)
     {
-        SpringApplication.run(Application.class,args);
+        SpringApplication.run(Application.class, args);
     }
 }
